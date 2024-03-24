08ae3bb6d3b7406f1424a8736090b79e04ca9ca6
sardor <frontendmii@gmail.com>
2024-03-24 08:10:35 +0500

    css index.html images node-modules pacage-lock and tailwind.config.js css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.{html,js}'],
+	theme: {
+		extend: {},
+		colors: {
+			white: '#fff',
+			secondary: '#ecf4ff',
+			blue: '#0d63f3',
+			black: '#000',
+			green: '#2cb708',
+			red: '#ff0101',
+			dark: '#1e1e1e',
+			gray: '#6c757d',
+			coral: '#eb8528',
+			'btn-blue': '#2f7af8',
+		},
+	},
+	plugins: [],
+}
